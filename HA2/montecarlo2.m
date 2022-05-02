0c4a5253f8d1e92f5d74bdeb9972dbf60846d85d
Axenmagnus <68119345+Axenmagnus@users.noreply.github.com>
2022-05-02 16:26:25 +0200

    Create montecarlo2.m

---
+++ HA2/montecarlo2.m
@@ -0,0 +1,6 @@
+clear all; close all; clc
+
+Iran_infected=readtable("iran_infected.csv"); Iran_recovered=readtable("iran_removed.csv");
+Germany_infected=readtable("germany_infected.csv");Germany_recovered=readtable("germany_removed.csv");
+Germany_population=83000000; Iran_population=84000000;
+phi=0.995; alpha=2;
