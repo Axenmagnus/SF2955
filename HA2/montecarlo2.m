db0d65e462e81768db47dcbf5779bf64d5f56952
Axenmagnus <Axenmagnus@users.noreply.github.com>
2022-05-05 12:29:52 +0200

    Test
    Test

--- HA2/montecarlo2.m
+++ HA2/montecarlo2.m
@@ -4,3 +4,4 @@ Iran_infected=readtable("iran_infected.csv"); Iran_recovered=readtable("iran_rem
 Germany_infected=readtable("germany_infected.csv");Germany_recovered=readtable("germany_removed.csv");
 Germany_population=83000000; Iran_population=84000000;
 phi=0.995; alpha=2;
+%%%CO
